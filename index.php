f3ea00a0898444c01a5747527b552b6034360d6a
minhnhat109 <81224264+minhnhat109@users.noreply.github.com>
2021-03-26 15:42:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div id="navigation">
+    <nav id="topmenu">
+        <ul>
+          <li><a href="#">Shopping</a></li>
+          <li><a href="#">My cart </a></li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="Register.html">Regiter</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="#">Logout</a></li>
+          <li><a href="#">Plan Page</a></li>
+        </ul>
+      </nav>
+    </div id ="header"> 
+    <P> <img src="1.jpg" >
+        </P>
+   
+        <section>
+          <article>
+            <h1> Áo Khoác</h1>
+            <img src="2.jpg" alt="">
+            <div class="price"> $200.000 </div>
+            <ul>
+              <li> Thông Tin Về áo khoác </li>
+              <li> Lựa chọn màu sắc </li>
+             
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+          <article>
+            <h1> áo thun</h1>
+            <img src="4.jpg" alt="">
+            <div class="price"> $500.000 </div>
+            <ul>
+              <li> Thông Tin Về áo thun </li>
+              <li> Lựa chọn màu sắc </li>
+                         
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+          <article>
+            <h1> túi </h1>
+            <img src="3.jpg" alt="">
+            <div class="price"> $200.000 </div>
+            <ul>
+              <li> Thông Tin Về Túi </li>
+              <li> lựa chọn màu sắc </li>
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+        </section>
